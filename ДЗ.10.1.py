5859660bbd3bde4192819fe564bba043eb4eea1f
Televiz0r <vf126675@gmail.com>
2023-04-24 21:10:59 +0700

    Решение задачи 10.1

---
+++ ДЗ.10.1.py
@@ -0,0 +1,12 @@
+# def min_to_sec():
+#     min = int(input('Введите целое количество минут: '))
+#     sec = min * 60
+#     return sec
+# print(min_to_sec())
+
+#Или
+
+# def min_to_sec(min):
+#     sec = min * 60
+#     return sec
+# print(min_to_sec(6))

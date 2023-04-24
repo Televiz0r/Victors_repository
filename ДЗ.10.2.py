d24514ea4eded6a08cbb501fa9d4d7115708d360
Televiz0r <vf126675@gmail.com>
2023-04-24 21:13:10 +0700

    Решение задачи 10.2

---
+++ ДЗ.10.2.py
@@ -0,0 +1,11 @@
+# def year_to_day():
+#     users_age = int(input('Сколько вам полных лет: ')) * 365
+#     return users_age
+# print(year_to_day())
+
+#Или
+
+# def year_to_day(users_age):
+#     users_age = users_age * 365
+#     return users_age
+# print(year_to_day(15))
